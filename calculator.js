d54dd866eaf4f20cc795c40b06addfcbf14742c1
Viccyace <adeviccy@gmail.com>
2024-05-03 19:58:15 -0200

    updated to be submitted as assignment

--- calculator.js
+++ calculator.js
@@ -1,12 +1,15 @@
 function display(value) {
     document.getElementById("result").value += value
 }
-function clearScreen() {
-    document.getElementById("result").value = ""
-}
+
 function calculate() {
     let x = document.getElementById("result").value
     let y = eval(x)
     document.getElementById("result").value = y
 }
+
+function clearScreen() {
+    document.getElementById("result").value = ""
+}
+
 console.log(calculate())
